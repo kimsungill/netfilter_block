f7e7067a3b58128d97f2bfbe72dc1ce515bfa1d5
kimsungill <kdsko123@naver.com>
2019-06-10 19:22:51 +0900

    Add nfqnl_test.c

---
+++ iptables.cpp
@@ -0,0 +1,6 @@
+#include "iptables.h"
+
+iptables::iptables()
+{
+
+}
---
+++ iptables.h
@@ -0,0 +1,11 @@
+#ifndef IPTABLES_H
+#define IPTABLES_H
+
+
+class iptables
+{
+public:
+    iptables();
+};
+
+#endif // IPTABLES_H
