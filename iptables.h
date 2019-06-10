bb028210db59a2789df83c5c1f971a949b275f14
kimsungill <kdsko123@naver.com>
2019-06-10 19:24:11 +0900

    Add iptables.h

--- iptables.h
+++ iptables.h
@@ -1,11 +1,26 @@
-#ifndef IPTABLES_H
-#define IPTABLES_H
+#include <stdio.h>
 
-
-class iptables
-{
-public:
-    iptables();
+struct ip_header{
+    unsigned char ip_ver_len;
+    unsigned char ip_ser_type;
+    unsigned short ip_pac_len;
+    unsigned short ip_ident;
+    unsigned short ip_offset;
+    unsigned char ip_live;
+    unsigned char ip_tran;
+    unsigned short ip_hea_sum;
+    unsigned int ip_s_address;
+    unsigned int ip_d_address;
 };
 
-#endif // IPTABLES_H
+struct tcp_header{
+    unsigned short tcp_s_port;
+    unsigned short tcp_d_port;
+    unsigned int tcp_seq_num;
+    unsigned int tcp_ack_num;
+    unsigned char tcp_offset_res;
+    unsigned char tcp_flags;
+    unsigned short tcp_win_size;
+    unsigned short tcp_checksum;
+    unsigned short tcp_urg_pointer;
+};
